3bdc0d9bca1aa8cece30d71f211d528c35873b85
Javi Ferrer <javi.f.o@gmail.com>
2016-04-26 20:12:28 +0200

    Add libswconverter and libcsc
    Change-Id: I5614306bcd2bf9560c9c7b20d0072cbd67a6a2c6

--- Android.mk
+++ Android.mk
@@ -25,7 +25,9 @@ exynos5_dirs := \
 	libscaler \
 	libgscaler \
 	libmpp \
-	libion_exynos
+	libion_exynos \
+	libswconverter \
+	libcsc
 
 include $(call all-named-subdir-makefiles,$(exynos5_dirs))
 
