f125f99fb56a4c7143f6ac06000b70d293dc65a8
Javi Ferrer <javi.f.o@gmail.com>
2016-05-05 18:54:23 +0200

    WIP: Add hwc
    ...and its dependent libraries
    
    PS8: Fix rotation and black frames with window animations
    PS9: Fix artifacts when rotating screen, compose more rgb layers, use unused MPP when needed for rgb composition
    PS10: It's like a backup. Mode switch has been added and tested, but stock doesn't use it.
    
    Status:
    - RGB composition working
    
    Change-Id: Ie4d6910534a199085a226417f56cc4f0f64dabc1

---
+++ libdisplay/ExynosDisplay.cpp
@@ -0,0 +1,64 @@
+#include "ExynosDisplay.h"
+#include "ExynosMPPModule.h"
+
+ExynosDisplay::ExynosDisplay(int numGSCs)
+{
+}
+
+ExynosDisplay::~ExynosDisplay()
+{
+}
+
+int ExynosDisplay::getDeconWinMap(int overlayIndex, int totalOverlays)
+{
+    if (totalOverlays <= 3)
+        return WINMAP0[overlayIndex];
+
+    if (totalOverlays == 4)
+        return WINMAP1[overlayIndex];
+
+    return overlayIndex;
+}
+
+enum decon_idma_type RGB_DMATYPE[] = {IDMA_G0, IDMA_G1, IDMA_G2, IDMA_G3};
+
+enum decon_idma_type ExynosDisplay::getDeconDMAType(enum mppLocality loc, int layerIdx)
+{
+    int result = -1;
+
+    switch(loc) {
+    case DECON_MPP_INT:
+        result = layerIdx + IDMA_VGR0;
+        break;
+
+    case DECON_MPP_EXT:
+        result = layerIdx + IDMA_VG0;
+        break;
+
+    case DECON_RGB:
+        result = RGB_DMATYPE[layerIdx];
+        break;
+    }
+
+    return (enum decon_idma_type) result;
+}
+
+enum decon_idma_type ExynosDisplay::getRGBDeconDMAType(int rgbLayerIdx)
+{
+    return RGB_DMATYPE[rgbLayerIdx];
+}
+
+int ExynosDisplay::prepare(hwc_display_contents_1_t *contents)
+{
+    ALOGD("%s: returning 0", __FUNCTION__);
+    return 0;
+}
+
+int ExynosDisplay::set(hwc_display_contents_1_t *contents)
+{
+    return 0;
+}
+
+void ExynosDisplay::freeMPP()
+{
+}
